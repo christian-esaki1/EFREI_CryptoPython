c73cf649403ba918a89a5698d7e19a7799f9724b
christian-esaki1 <christian.esaki@gmail.com>
2025-05-21 12:46:30 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,6 +19,7 @@ def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-                                                                                                                                                     
+
+
 if __name__ == "__main__":
   app.run(debug=True)
